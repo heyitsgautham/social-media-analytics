c9fb7a90516fb36d98d75925480d83419c6debc3
Gautham Krishna <heyitsgautham@gmail.com>
2025-09-20 12:05:03 +0530

    chore(seed): Faker dataset + Typer CLI (users, posts, tags, comments, engagements)

--- app/db.py
+++ app/db.py
@@ -1,7 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from contextlib import contextmanager
 
 from app.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+@contextmanager
+def get_session() -> Session:
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
